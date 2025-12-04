c7e9ffbe94959c12c537f0840f0b2754376d059f
Eonebyte <eonebyte@gmail.com>
2025-12-04 13:57:55 +0700

    repair product use like

--- web_service/server.js
+++ web_service/server.js
@@ -387,12 +387,24 @@ fastify.post('/api/validate-sales-order', async (request, reply) => {
                             );
 
                             if (resProduct.rows.length === 0) {
-                                validationErrors.push({
-                                    ...lineLocation,
-                                    field: 'Product',
-                                    value: partNo,
-                                    message: `Product / Part No "${partNo}" tidak ditemukan.`
-                                });
+                                const resProductLike = await connection.execute(
+                                    `   SELECT p.M_PRODUCT_ID FROM M_Product p
+                                        WHERE LOWER(Value) LIKE '%' || LOWER(:value) || '%'
+                                        AND p.M_PRODUCT_CATEGORY_ID = 1000000 AND p.ISMOULD = 'N'`,
+                                    { value: partNo },
+                                    { outFormat: oracleDB.instanceOracleDB.OUT_FORMAT_OBJECT }
+                                );
+
+                                if (resProductLike.rows.length === 0) {
+                                    validationErrors.push({
+                                        ...lineLocation,
+                                        field: 'Product',
+                                        value: partNo,
+                                        message: `Product / Part No "${partNo}" tidak ditemukan.`
+                                    });
+                                } else {
+                                    line.m_product_id = resProductLike.rows[0].M_PRODUCT_ID;
+                                }
                             } else {
                                 line.m_product_id = resProduct.rows[0].M_PRODUCT_ID;
                             }
